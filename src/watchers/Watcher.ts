046ee738f3665e28c3171315f236b048f9642dba
deejayy <deejayy@deejayy.hu>
2018-09-13 22:01:03 +0200

    Prepare handling nested metrics (like diskfree)

--- src/watchers/Watcher.ts
+++ src/watchers/Watcher.ts
@@ -1,6 +1,8 @@
 import { BehaviorSubject } from "rxjs";
 import { MetricOptions } from "@models/MetricOptions";
 import { MetricValue } from "@models/MetricValue";
+import { MetricSeparator } from "@models/MetricSeparator";
+import { isArray } from "util";
 
 export class Watcher {
   public metric: any;
@@ -17,7 +19,13 @@ export class Watcher {
   schedule(stream: BehaviorSubject<MetricValue>) {
     this.timer = setInterval(() => {
       let currentValue: MetricValue = new MetricValue(this.metricName, this.metric.getCurrentValue());
-      stream.next(currentValue);
+      if (isArray(currentValue.value)) {
+        currentValue.value.map(singleMetric => {
+          stream.next(new MetricValue(currentValue.name + MetricSeparator + singleMetric.name, singleMetric.value));
+        });
+      } else {
+        stream.next(currentValue);
+      }
     }, this.options.interval);
   }
 
