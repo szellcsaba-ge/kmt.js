f61d45c178c005d0fa5074fdd9e5de458e8f2391
deejayy <deejayy@deejayy.hu>
2018-09-13 16:21:07 +0200

    Add different emit methods to SocketOConsumer

--- src/consumers/SocketIOConsumer.ts
+++ src/consumers/SocketIOConsumer.ts
@@ -1,9 +1,16 @@
 import { MetricValue } from '@models/MetricValue';
 import * as socketio from 'socket.io';
 
+export enum EmitMethod {
+  last = 'LAST',
+  average = 'AVERAGE',
+  sum = 'SUM',
+}
+
 export class SocketIOConsumerOptions {
   port: number = 8012;
   interval: number = 5000;
+  emitMethod: EmitMethod = EmitMethod.sum;
 };
 
 export class SocketIOConsumer {
@@ -12,21 +19,47 @@ export class SocketIOConsumer {
   public options: SocketIOConsumerOptions = {
     port: 8012,
     interval: 5000,
+    emitMethod: EmitMethod.sum,
   }
 
-  constructor(options: { port?: number, interval?: number } = {}) {
+  constructor(options:
+    {
+      port?: number,
+      interval?: number,
+      emitMethod?: EmitMethod
+    } = {}
+  ) {
     this.options.port = options.port || this.options.port;
     this.options.interval = options.interval || this.options.interval || 0;
+    this.options.emitMethod = options.emitMethod || this.options.emitMethod || 0;
 
     this.io = socketio.default.listen(this.options.port);
     setInterval(() => { this.deliver() }, this.options.interval);
   }
 
   consume(value: MetricValue) {
-    this.metrics[value.name] = value;
+    if (this.options.emitMethod == EmitMethod.last) {
+      this.metrics[value.name] = value;
+    }
+    if (this.options.emitMethod == EmitMethod.average || this.options.emitMethod == EmitMethod.sum) {
+      this.metrics[value.name] = [ ... this.metrics[value.name] || [], value ];
+    }
   }
 
   deliver() {
-    this.io.emit('deliver', this.metrics);
+    if (this.options.emitMethod == EmitMethod.last || this.options.emitMethod == EmitMethod.sum) {
+      this.io.emit('deliver', [ this.metrics ]);
+    }
+    if (this.options.emitMethod == EmitMethod.average) {
+      const emitMetrics: any = {};
+      Object.keys(this.metrics).forEach((value: string) => {
+        emitMetrics[value] = this.metrics[value].reduce((acc: MetricValue, curr: MetricValue, index: number, arr: MetricValue[]) => {
+          acc.value += curr.value / arr.length;
+          return acc;
+        }, { value: 0 });
+      });
+      this.io.emit('deliver', [ emitMetrics ]);
+    }
+    this.metrics = {};
   }
 }
